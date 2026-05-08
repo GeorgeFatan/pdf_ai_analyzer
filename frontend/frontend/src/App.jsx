390ef7125dc6e97251b7704004a1e78e0dd88aa6
GeorgeFatan <127293166+GeorgeFatan@users.noreply.github.com>
2026-05-08 21:20:06 +0300

    Added  the upload doc funct

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -5,6 +5,32 @@ function App() {
   const [question, setQuestion] = useState("");
   const [messages, setMessages] = useState([]);
 
+
+// file upload function
+  const handleFileUpload = async (e) =>{
+    const file = e.target.files[0]; 
+    if(!file) return;
+
+    const formData = new FormData(); // form data pentru upload
+    formData.append("file", file); // adauga fisierul in form data
+
+    try {
+      const response = await fetch("http://127.0.0.1:8000/upload-pdf/", {
+        method: "POST",
+        body: formData
+      });
+
+      const data = await response.json();
+      console.log("File uploaded is: ", data);
+
+      setMessages(prev => [...prev, {role: "system", content: `File ${data.filename}" uploaded successfully!`
+      }]);
+    } catch (error){
+      console.error("error uploading file: ",error);
+    }
+  };
+
+  // submit chat function
   const handleSubmit = async () => {
     if (!question.trim()) return;
 
@@ -38,6 +64,8 @@ function App() {
     <div className="app-container">
       <h1>AI DOCUMENT ANALYZER</h1>
 
+      <input type="file" onChange={handleFileUpload} />
+
       <div className="chat-container">
         {messages.map((msg, i) => (
           <div key={i} className={`msg ${msg.role}`}>
@@ -57,6 +85,8 @@ function App() {
       </div>
     </div>
   );
+
+
 }
 
 export default App;
